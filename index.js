9f27d714b40e49b7fe2626961c3508ac4fc2cdeb
Yuxiang Li <seanliyuxiang@gmail.com>
2021-09-11 13:38:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+function submitData(userName, userEmail) {
+  
+  // data payload
+  const formData = {
+    name: userName,
+    email: userEmail
+  };
+
+  const configObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch('http://localhost:3000/users', configObj)
+  .then(response => response.json())
+  .then(json => {
+    let h1 = document.createElement('h1');
+    h1.innerHTML = json.id;
+    document.querySelector('body').appendChild(h1);
+  })
+  .catch(error => {
+    let h2 = document.createElement('h2');
+    h2.innerHTML = error.message;
+    document.querySelector('body').appendChild(h2);
+  });
+}
